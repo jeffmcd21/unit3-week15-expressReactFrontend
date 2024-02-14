e70d7fa87f5a480e7969334795957deab34aefde
Jeff McDonald <jwmcdonald85@gmail.com>
2024-02-13 19:55:19 -0500

    update styling with scss

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -27,8 +27,6 @@ const Index = () => {
                     <Link to={`/${person._id}`}>
                         <h1>{person.name}</h1>
                     </Link>
-
-                    <h1>{person.name}</h1>
                     <img src={person.image} alt={person.name}/>
                     <h3>{person.title}</h3>
                 </div>
@@ -40,36 +38,3 @@ const Index = () => {
 }
 
 export default Index;
-
-
-
-
-
-// function Index(props) {
-
-
-//   return (
-//     <div>
-//       <h2>Create a Person</h2>
-//       <Form action="/create" method="post">
-//         <input type="input" name="name" placeholder="person's name" />
-//         <input type="input" name="image" placeholder="person's picture" />
-//         <input type="input" name="title" placeholder="person's title" />
-//         <input type="submit" value="create person" />
-//       </Form>
-
-//       <h2>People</h2>
-//       {people.map(person => (
-//         <div key={person._id} className="person">
-//           <Link to={`/${person._id}`}>
-//             <h1>{person.name}</h1>
-//           </Link>
-//           <img src={person.image} alt={person.name} />
-//           <h3>{person.title}</h3>
-//         </div>
-//       ))}
-//     </div>
-//   )
-// }
-
-// export default Index
